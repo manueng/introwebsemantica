1230fdf7bceb02e2d91b25fcf24c4da399bb0642
Emanuel <manusenac@hotmail.com>
2018-11-26 21:54:51 -0200

    triplasrdf
    primeiro commit

---
+++ banco de triplas.py
@@ -0,0 +1,6 @@
+from rdflib import URIRef, BNode, Literal, Namespace, RDF, Graph
+from SPARQLWrapper import SPARQLWrapper, JSON
+g = Graph()
+g.parse("/home/usuario/Downloads/trabalho web semantica/filiados_dem_ac-output-minimal.rdf")
+
+
