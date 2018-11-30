d67f1d2a1c43dac10854b8192e9a40c08ea0ff96
emanueng <emanuelhbga@gmail.com>
2018-11-30 19:18:10 -0200

    inserindo triplas no banco

--- banco de triplas.py
+++ banco de triplas.py
@@ -1,6 +1,22 @@
 from rdflib import URIRef, BNode, Literal, Namespace, RDF, Graph
 from SPARQLWrapper import SPARQLWrapper, JSON
+import rdfextras
 g = Graph()
-g.parse("/home/usuario/Downloads/trabalho web semantica/filiados_dem_ac-output-minimal.rdf")
+g.parse("/home/usuario/.cache/.fr-nCZyvZ/file-output-minimal.rdf")
 
+print(len(g))
+rdfextras.registerplugins()
+
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery("""
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?label
+    WHERE { <http://dbpedia.org/resource/Carro> rdfs:label ?label }
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    print(result["label"]["value"])
 
